1085937048b0ed8535402bfa935409040ca6b8c7
Akbar <md.akbarahmed786@gmail.com>
2021-02-09 14:28:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -367,7 +367,7 @@ function speak_impl(voice_Connection, mapKey) {
             const duration = buffer.length / 48000 / 4;
             console.log("duration: " + duration)
 
-            if (duration < 1.0 || duration > 19) { // 20 seconds max dur
+            if (duration < 1.0 || duration > 6) { // 20 seconds max dur
                 console.log("TOO SHORT / TOO LONG; SKPPING")
                 return;
             }
