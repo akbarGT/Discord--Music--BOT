c9a7357d2e3c01202b9e2d6f36c4fb4b0f344259
Akbar <md.akbarahmed786@gmail.com>
2021-02-09 15:38:11 +0530

    tts true no space

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ discordClient.on('message', async (msg) => {
                 if (val.voice_Connection) val.voice_Connection.disconnect()
                 if (val.musicYTStream) val.musicYTStream.destroy()
                     guildMap.delete(mapKey)
-                msg.reply("Disconnected.", {  tts: true })
+                msg.reply("Disconnected.", {tts: true})
             } else {
                 msg.reply("Cannot leave because not connected.")
             }
