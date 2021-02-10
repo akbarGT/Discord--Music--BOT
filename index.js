981f5e3406d57293479de96b958dd0dba636f9d6
Akbar <md.akbarahmed786@gmail.com>
2021-02-10 20:44:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -258,7 +258,7 @@ discordClient.on('message', async (msg) => {
                 val.debug = true;
         }
         else if (msg.content.trim().toLowerCase() == _CMD_TEST) {
-            msg.reply('hello back =)')
+            msg.reply('Melody online. All functions working.')
         }
         else if (msg.content.split('\n')[0].split(' ')[0].trim().toLowerCase() == _CMD_LANG) {
             const lang = msg.content.replace(_CMD_LANG, '').trim().toLowerCase()
