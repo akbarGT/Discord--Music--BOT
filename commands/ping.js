c95a68e30c3c795197affca9857956db8b3c7191
Akbar <md.akbarahmed786@gmail.com>
2021-02-11 20:32:10 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'ping',
     description: "this is a ping command!",
-    execute(message, args){
-        message.channel.send('pong!');
+    execute(msg, args){
+        msg.channel.send('pong!');
     }
 }
