887b5a0982da76732f4a1fd810cb055afb0e2817
Akbar <md.akbarahmed786@gmail.com>
2021-02-11 22:18:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -284,6 +284,8 @@ discordClient.on('message', async (msg) => {
         else if (msg.content.trim().toLowerCase() == _CMD_DEL10) {
             msg.channel.bulkDelete(11);
             msg.reply(`**Successfully** Deleted ***10*** Messages.`)
+            .then(msg =>
+                msg.delete(5000))
         }
         else if (msg.content.split('\n')[0].split(' ')[0].trim().toLowerCase() == _CMD_LANG) {
             const lang = msg.content.replace(_CMD_LANG, '').trim().toLowerCase()
