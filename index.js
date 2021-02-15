346a57114b5d99bc31fb6c3677035a99f2ad5c52
Akbar <md.akbarahmed786@gmail.com>
2021-02-16 04:54:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -768,17 +768,10 @@ async function music_message(message, mapKey) {
 
     queueTryPlayNext(mapKey, (title) => {
         message.react(EMOJI_PLAY);
-        message.channel.bulkDelete(2, true)
         message.channel.send('Now playing: **' + title + '**', { tts: true })
         .then(msg => {
             msg.delete({
                 timeout: 1000 * 25
-            })
-        });
-        message.channel.send(`.rr`)
-        .then(msg => {
-            msg.delete({
-                timeout: 500
             })
         });
     }, (msg) => {
